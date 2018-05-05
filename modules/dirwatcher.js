067979ae647091851147d0650bc8f072fa83128d
Mikhail Yamayeu <mikhail_yamayeu@epam.com>
2018-05-05 13:32:29 +0300

    fixed console.log

--- modules/dirwatcher.js
+++ modules/dirwatcher.js
@@ -2,7 +2,6 @@ import fs from "fs";
 import path from "path";
 import crypto from "crypto";
 
-console.log(path.resolve);
 export default class DirWatcher {
 	constructor({ emitter }) {
 		this.emitter = emitter;
